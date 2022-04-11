6f733431a5c27c715ce62ce53530b5a77fd80803
Mustafa Assaf <mustafakassaf@gmail.com>
2022-04-11 23:11:09 +0300

    add setup

--- test_calculatorApp.py
+++ test_calculatorApp.py
@@ -2,6 +2,8 @@ import unittest
 from calculatorApp import *
 
 class TestCalculate(unittest.TestCase):
+    def setUp(self):
+         print("Setup .. ")
 
     def test_AddPass(self):
         self.assertEqual(calculate('1',2,3), 5)
@@ -17,6 +19,9 @@ class TestCalculate(unittest.TestCase):
     def test_DividByZerrorRegex(self):
         with self.assertRaisesRegex(ValueError, "input is not a number!"):
              calculate('4','3','w')
+
+    def tearDown(self):
+        print("tearDown .. ")
 
 if __name__ == '__main__':
 	unittest.main()
