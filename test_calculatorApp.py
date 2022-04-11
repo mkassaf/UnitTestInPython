a4918c8605f5f30edc8ccd9b4b08939b24223cc7
Mustafa Assaf <mustafakassaf@gmail.com>
2022-04-11 17:48:24 +0300

    add more example

--- test_calculatorApp.py
+++ test_calculatorApp.py
@@ -1,7 +1,7 @@
 import unittest
 from calculatorApp import *
 
-class TestStringMethods(unittest.TestCase):
+class TestCalculate(unittest.TestCase):
 
     def test_AddPass(self):
         self.assertEqual(calculate('1',2,3), 5)
@@ -10,9 +10,13 @@ class TestStringMethods(unittest.TestCase):
         self.assertNotEqual(calculate('1',2,3), 9)
 
     def test_DividByZerror(self):
-         with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError):
              calculate('4','3','w')
+        ##Or you can use this ##self.assertRaises(ValueError, calculate, '4','3','w') 
 
+    def test_DividByZerrorRegex(self):
+        with self.assertRaisesRegex(ValueError, "input is not a number!"):
+             calculate('4','3','w')
 
 if __name__ == '__main__':
 	unittest.main()
